cd28cfc7503ef60a01aad8f4e1f994f05d6bc5e3
geekyprince <arch.prince01@gmail.com>
2020-02-06 06:47:25 +0000

    ME_QBOX_MODULE_SERVER_CONFIGURATION

--- server_configuration/check_QBox_server_cpu.sh
+++ server_configuration/check_QBox_server_cpu.sh
@@ -20,7 +20,7 @@ requiredCPUSpeedInMHz=1000     # 1 GHz
     # 2) Start the expression to populate the variable with "cat $1" which is the command line filename input. Check the Usage above. 
     # 3) Bash script can only do integer arithmetic. Include additional handling needed to strip the decimal part
     # 4) Execute the script before submission and confirm the output 
-cpuSpeedInMHz="`cat /proc/cpuinfo | grep -i mhz |uniq|cut -d':' -f2|cut -d'.' -f1`"
+cpuSpeedInMHz="`cat /proc/cpuinfo | grep -i MHz |uniq| awk '{print int($4)}'`"
 if test $cpuSpeedInMHz -ge $requiredCPUSpeedInMHz
    then
    echo "CPU Speed of $cpuSpeedInMHz MHz is sufficient for QBox"
